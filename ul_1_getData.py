c448e4e8669e604d9a0bc008fa1ffefc9b6faa05
Colin Roos <colin.a.roos@gmail.com>
2020-01-20 17:08:57 -0500

    Implement request by stationID.

--- ul_1_getData.py
+++ ul_1_getData.py
@@ -4,24 +4,43 @@ import pandas as pd
 from util import get_weather
 
 # NOAA API Token
-
 myToken = 'oGjWvAZgOEWogyaRmrdJxJgyCnWwTMFq'
+# Path to location list
+station_path = 'data/city_codes.csv'
 
 # get datetime of last year
-lastyear = datetime.datetime.now() - datetime.timedelta(weeks=2)
+lastyear = datetime.datetime.now() - datetime.timedelta(weeks=52)
+lastyear_plus = lastyear + datetime.timedelta(weeks=2)
 
 # set start and end dates, currently set to 1 day's data for testing
 begin_date = lastyear.strftime("%Y-%m-%d")
-end_date = lastyear.strftime("%Y-%m-%d")
+end_date = lastyear_plus.strftime("%Y-%m-%d")
+
+# import list of locations
+df_stations = pd.read_csv(station_path)
+stations = df_stations.StationID
+print('Fetching the following stations...')
+print(df_stations)
 
 # location id for the locations of interest
 locationid = 'FIPS:38'
+stationid = 'GHCND:MXM00076680'
 datasetid = 'GHCND'
 
 # urls
 base_url_data = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
-base_url_stations = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations'
+df_weather = pd.DataFrame()
 
-df_weather = get_weather(locationid, datasetid, begin_date, end_date, myToken, base_url_data)
+for s in stations:
+    d = get_weather(stationid=s, datasetid=datasetid, datatype='PRCP', start_date=begin_date, end_date=end_date,
+                    token=myToken, base_url=base_url_data)
+    df_weather = df_weather.append(d, ignore_index=True)
 
-print(df_weather.head())
+# separate for PRCP data
+prcp = df_weather[df_weather['datatype'] == 'PRCP']
+tavg = df_weather[df_weather['datatype'] == 'PRCP']
+
+prcp = prcp.join(tavg, on=['date', 'station'], rsuffix='_1')
+
+print(prcp)
+# df_weather.to_csv('out/mx_test.csv')
--- util.py
+++ util.py
@@ -2,11 +2,11 @@ import requests
 import pandas as pd
 
 
-def get_weather(locationid, datasetid, begin_date, end_date, myToken, base_url):
-    token = {'token': myToken}
+def get_weather(datasetid, stationid, datatype, start_date, end_date, token, base_url, locationid=''):
+    token = {'token': token}
 
-    params = 'datasetid=' + str(datasetid) + '&' + 'locationid=' + str(locationid) + '&' + 'startdate=' + str(
-        begin_date) + '&' + 'enddate=' + str(end_date) + '&' + 'limit=25' + '&' + 'units=standard'
+    params = 'datasetid=' + str(datasetid) + '&' + 'datatypeid' + str(datatype) + '&' + 'stationid=' + str(stationid) + '&' + 'startdate=' + str(
+        start_date) + '&' + 'enddate=' + str(end_date) + '&' + 'limit=500' + '&' + 'units=metric'
 
     r = requests.get(base_url, params=params, headers=token)
     print('Request status code: ' + str(r.status_code))
