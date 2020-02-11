1ccdf16df9c5d46c59f35d5e1283f33b4b442235
Colin Roos <colin.a.roos@gmail.com>
2020-02-10 20:23:02 -0500

    Normalize weather data.

--- ul_3a_featureEng.py
+++ ul_3a_featureEng.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
 
+pd.set_option('display.width', 2000)
+pd.set_option('display.max_columns', 500)
 # Filepaths
 wdata_path = 'data/cleaned_weather_data.csv'
 
@@ -23,12 +26,13 @@ df = df_w.merge(daily_norm, how='left', left_on=df_w.index.dayofyear, right_on=d
 df.index = df_w.index
 df.drop('key_0', axis=1, inplace=True)
 
-labels = df.iloc[:, :24].add_prefix('diff').columns.values
-for i in np.arange(0, 24):
-    df[labels[i]] = df.iloc[:, i] - df.iloc[:, i + 24]
+labels = df.iloc[:, :22].add_prefix('diff').columns.values
+for i in np.arange(0, 22):
+    df[labels[i]] = df.iloc[:, i] - df.iloc[:, i + 22]
 
 print(df.info())
-df[['tavg_moore', 'normtavg_moore', 'difftavg_moore']].plot(linewidth=0.5)
+df[['difftavg_tor', 'difftavg_lake', 'difftavg_moore', 'difftavg_orlan', 'difftavg_nyc']].plot(linewidth=0.5, alpha=0.8,
+                                                                                               grid=True)
 plt.show()
 
 # imput both NaNs and '-' symbols
@@ -39,7 +43,14 @@ imp = SimpleImputer(missing_values='-', strategy='constant', fill_value=0)
 # impute
 df.iloc[:, :11] = nan_imp.fit_transform(df.iloc[:, :11])
 df.iloc[:, 11:] = nan_mean.fit_transform(df.iloc[:, 11:])
-print(df.info())
+print(df.describe())
 
+# normalize
+scl = StandardScaler()
+df_s = pd.DataFrame(scl.fit_transform(df))
+df_s.columns = df.columns
+df_s.index = df.index
+
+print(df_s.describe())
 # write to file
-df.to_csv('out/master_weather.csv')
+df.to_csv('out/master_weather_norm.csv')
