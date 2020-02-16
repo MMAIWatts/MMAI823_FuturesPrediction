45555a0eaaef26516241f7d9853e93dffc557935
Colin Roos <colin.a.roos@gmail.com>
2020-02-16 14:12:21 -0500

    Implement series_to_supervised.

--- ul_3b_jointargets.py
+++ ul_3b_jointargets.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from sklearn.impute import SimpleImputer
+from util import series_to_supervised
 
 # set Pandas options
 pd.set_option('display.width', 2000)
@@ -29,11 +30,7 @@ print(df_f.info())
 df = df_w.merge(df_f, how='left', left_index=True, right_index=True)
 
 # imput missing values for targets
-nan_imp = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0)
-
-# impute finance targets
-df.iloc[:, -65:] = nan_imp.fit_transform(df.iloc[:, -65:])
-print(df.describe())
+df = df.fillna(0, axis=0)
 
 print(df.head(20))
 data = df.iloc[:, :-65]
@@ -41,22 +38,25 @@ data.index = pd.to_datetime(data.index)
 targets = df.iloc[:, -65:]
 targets.index = pd.to_datetime(targets.index, )
 
-X, y = list(), list()
+n_lag = 40
+n_seq = 10
 
-for i, row in df_dates.iterrows():
-    cstart = datetime.datetime.fromisoformat(row['Start'])
-    end = datetime.datetime.fromisoformat(row['End'])
-    # create batches for train-test
-    if row['Contract'][0] != 'H':
-        np.savetxt('out/X_Htrain.csv' , X, delimiter=',')
-        break
-    for x in range(len(data)):
-        start = cstart + datetime.timedelta(days=2*x)
-        in_e = start + datetime.timedelta(days=40)
-        out_s = start + datetime.timedelta(days=40)
-        out_e = out_s + datetime.timedelta(days=10)
-        if out_e <= end:
-            X.append(data.loc[start:in_e, :].to_numpy())
-            y.append(targets.loc[out_s:out_e, row['Contract']].to_numpy())
-        else:
-            break
+supervised = series_to_supervised(df, n_in=n_lag, n_out=n_seq, dropnan=True)
+
+# for i, row in df_dates.iterrows():
+#     cstart = datetime.datetime.fromisoformat(row['Start'])
+#     end = datetime.datetime.fromisoformat(row['End'])
+#     # create batches for train-test
+#     if row['Contract'][0] != 'H':
+#         np.savetxt('out/X_Htrain.csv', X, delimiter=',')
+#         break
+#     for x in range(len(data)):
+#         start = cstart + datetime.timedelta(days=2*x)
+#         in_e = start + datetime.timedelta(days=40)
+#         out_s = start + datetime.timedelta(days=40)
+#         out_e = out_s + datetime.timedelta(days=10)
+#         if out_e <= end:
+#             X.append(data.loc[start:in_e, :].to_numpy())
+#             y.append(targets.loc[out_s:out_e, row['Contract']].to_numpy())
+#         else:
+#             break
--- util.py
+++ util.py
@@ -3,6 +3,19 @@ import pandas as pd
 
 
 def get_weather(datasetid, stationid, start_date, end_date, token, base_url, locationid=''):
+    """
+    Fetches weather data from NOAA API.
+
+    :param datasetid: Dataset to fetch from
+    :param stationid: Station ID of weather to fetch
+    :param start_date: Start date of fetch in ISO format (YYYY-MM-DD)
+    :param end_date: End date of fetch in ISO format (YYYY-MM-DD)
+    :param token: API Token as dict
+    :param base_url: Base url of database to fetch
+    :param locationid: optional location ID, station ID overrides
+    :return: pandas DataFrame containing results.
+    """
+
     token = {'token': token}
 
     params = 'datasetid=' + str(datasetid) + '&' + 'stationid=' + str(stationid) + '&' + 'startdate=' + str(
@@ -22,3 +35,35 @@ def get_weather(datasetid, stationid, start_date, end_date, token, base_url, loc
     except:
         print('Error converting weather data to DataFrame.')
 
+
+def series_to_supervised(data, targets, n_in=1, n_out=1, dropnan=True):
+    """
+    Converts a time series to a supervised learning problem.
+    :param data: Input data
+    :param targets: Output targets
+    :param n_in: Number of input time steps
+    :param n_out: Number of output time steps
+    :param dropnan: Drop NaN flag
+    :return: Aggregated data as a pandas DataFrame
+    """
+
+    n_vars = 1 if type(data) is list else data.shape[1]
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(data.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(targets.iloc[:, 0].shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars-1,n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars-1,n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
